c55ede83acbb79e79c993c4484820451b885c94f
junsuk123 <bob4587@naver.com>
2026-03-23 14:13:45 +0900

    feat: 데이터 수집 설정 업데이트 및 새로운 데이터셋 추가

--- src/sjtu_drone-ros2/matlab/AutoSimMain.m
+++ src/sjtu_drone-ros2/matlab/AutoSimMain.m
@@ -8,8 +8,8 @@ mainCfg = struct();
 
 % Data collection settings (editable in main).
 mainCfg.collection = struct();
-mainCfg.collection.scenario_count = 100;
-mainCfg.collection.drone_count = 4;
+mainCfg.collection.scenario_count = 500;
+mainCfg.collection.drone_count = 2;
 mainCfg.collection.independent_per_drone = true;
 mainCfg.collection.merge_last_runs = 5;
 mainCfg.collection.launch_use_gui = false;
